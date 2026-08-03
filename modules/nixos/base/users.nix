e04aed7653babfa0cc3522105bf3bdd0409babb0
7mochi <flyingcatdm@gmail.com>
2026-08-03 17:42:57 -0500

    feat(users): declare nanamochi password and set mutableUsers

--- modules/nixos/base/users.nix
+++ modules/nixos/base/users.nix
@@ -18,8 +18,11 @@ _:
       };
     in
     {
-      users.users = {
-        nanamochi = normalUser "nanamochi";
+      users.mutableUsers = false;
+
+      users.users.nanamochi = normalUser "nanamochi" // {
+        uid = 1000;
+        hashedPassword = "$6$U8BFYn0uRr9YVvoO$nZg4.eDjSaHyZo1wYlxjfylSj0qHFr4kVNSEdBtzahWHdqeBFou/Xck/0KYnzoZLuYXNewHQqpH0fYqYTOS9b1";
       };
 
       programs.fish.enable = true;
