1deb95c3e0dc54834da7f24f78420e05be76e2fa
7mochi <flyingcatdm@gmail.com>
2026-08-05 11:13:48 -0500

    feat(home): add tokyo night theme to opencode

--- modules/home/shared/programs/opencode.nix
+++ modules/home/shared/programs/opencode.nix
@@ -2,15 +2,24 @@ _:
 
 {
   flake.modules.homeManager.shared = {
-    home.file.".config/opencode/opencode.json".text = ''
-      {
-        "$schema": "https://opencode.ai/config.json",
-        "provider": {
-          "opencode": {
-            "options": { "apiKey": "{file:/run/secrets/opencode-api-key}" }
+    xdg.configFile = {
+      "opencode/opencode.json".text = ''
+        {
+          "$schema": "https://opencode.ai/config.json",
+          "provider": {
+            "opencode": {
+              "options": { "apiKey": "{file:/run/secrets/opencode-api-key}" }
+            }
           }
         }
-      }
-    '';
+      '';
+
+      "opencode/tui.json".text = ''
+        {
+          "$schema": "https://opencode.ai/tui.json",
+          "theme": "tokyonight"
+        }
+      '';
+    };
   };
 }
