e248c7186c52d4565b6da793cb55ca3ac7b028b5
7mochi <flyingcatdm@gmail.com>
2026-08-24 23:58:20 -0500

    style: nest home keys to satisfy statix

--- modules/home/shared/programs/jetbrains.nix
+++ modules/home/shared/programs/jetbrains.nix
@@ -5,29 +5,31 @@
     { pkgs, ... }:
 
     {
-      home.packages = [
-        (inputs.nix-jetbrains-plugins.lib.buildIdeWithPlugins pkgs "idea" [
-          "izhangzhihao.rainbow.brackets"
-          "com.junkfactory.tokyodark"
-          "String Manipulation"
-          "com.github.copilot"
-        ])
-      ];
+      home = {
+        packages = [
+          (inputs.nix-jetbrains-plugins.lib.buildIdeWithPlugins pkgs "idea" [
+            "izhangzhihao.rainbow.brackets"
+            "com.junkfactory.tokyodark"
+            "String Manipulation"
+            "com.github.copilot"
+          ])
+        ];
 
-      home.file.".config/JetBrains/IntelliJIdea2026.2/options/colors.scheme.xml".text = ''
-        <application>
-          <component name="EditorColorsManagerImpl">
-            <global_color_scheme name="TokyoDark" />
-          </component>
-        </application>
-      '';
+        file.".config/JetBrains/IntelliJIdea2026.2/options/colors.scheme.xml".text = ''
+          <application>
+            <component name="EditorColorsManagerImpl">
+              <global_color_scheme name="TokyoDark" />
+            </component>
+          </application>
+        '';
 
-      home.file.".config/JetBrains/IntelliJIdea2026.2/options/laf.xml".text = ''
-        <application>
-          <component name="LafManager">
-            <laf themeId="com.junkfactory.tokyodark" />
-          </component>
-        </application>
-      '';
+        file.".config/JetBrains/IntelliJIdea2026.2/options/laf.xml".text = ''
+          <application>
+            <component name="LafManager">
+              <laf themeId="com.junkfactory.tokyodark" />
+            </component>
+          </application>
+        '';
+      };
     };
 }
